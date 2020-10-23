b2d6592699d462e34487c5c80d6fca77218b76b3
yivan <yivannaviy@gmail.com>
2020-10-23 22:44:15 +0900

    pairwise similarity + triplet comparison

--- lio/losses/classification.py
+++ lio/losses/classification.py
@@ -1,4 +1,4 @@
-from typing import Callable
+from typing import Callable, List
 
 import torch
 import torch.nn.functional as F
@@ -11,6 +11,42 @@ def indirect_observation_loss(transition_matrix: torch.Tensor, activation: Calla
     def loss(t: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
         p_z = activation(t)
         p_y = p_z @ transition_matrix.to(y.device)
+        return F.nll_loss(torch.log(p_y + 1e-32), y)
+
+    return loss
+
+
+def pairwise_similarity_loss(activation: Callable = None) -> Callable:
+    if activation is None:
+        activation = lambda t: F.softmax(t, dim=1)
+
+    def loss(ts: List[torch.Tensor], y: torch.Tensor) -> torch.Tensor:
+        t1, t2 = ts
+        p_z1 = activation(t1)
+        p_z2 = activation(t2)
+        p_y1 = (p_z1 * p_z2).sum(1)
+        p_y0 = 1. - p_y1
+        p_y = torch.stack((p_y0, p_y1), dim=1)
+        return F.nll_loss(torch.log(p_y + 1e-32), y)
+
+    return loss
+
+
+def triplet_comparison_loss(activation: Callable = None) -> Callable:
+    if activation is None:
+        activation = lambda t: F.softmax(t, dim=1)
+
+    def loss(ts: List[torch.Tensor], y: torch.Tensor) -> torch.Tensor:
+        t1, t2, t3 = ts
+        p_z1 = activation(t1)
+        p_z2 = activation(t2)
+        p_z3 = activation(t3)
+        p_z12 = (p_z1 * p_z2).sum(1)
+        p_z13 = (p_z1 * p_z3).sum(1)
+        p_y1 = p_z12 * (1. - p_z13)
+        p_y2 = (1. - p_z12) * p_z13
+        p_y0 = 1. - p_y1 - p_y2
+        p_y = torch.stack((p_y0, p_y1, p_y2), dim=1)
         return F.nll_loss(torch.log(p_y + 1e-32), y)
 
     return loss
