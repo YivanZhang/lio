d799d01a20428db130507202f839ac619dbc3b81
yivan <yivannaviy@gmail.com>
2020-10-23 21:58:35 +0900

    softmax dimension

--- lio/losses/classification.py
+++ lio/losses/classification.py
@@ -6,10 +6,10 @@ import torch.nn.functional as F
 
 def indirect_observation_loss(transition_matrix: torch.Tensor, activation: Callable = None) -> Callable:
     if activation is None:
-        activation = F.softmax
+        activation = lambda t: F.softmax(t, dim=1)
 
     def loss(t: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
-        p_z = activation(t, dim=1)
+        p_z = activation(t)
         p_y = p_z @ transition_matrix.to(y.device)
         return F.nll_loss(torch.log(p_y + 1e-32), y)
 
