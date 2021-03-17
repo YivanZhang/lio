dd26e33dae49452413a4ecc64b32855881399f32
yivan <yivannaviy@gmail.com>
2021-03-17 20:38:37 +0900

    bootstrapping, focal, and gce

--- lio/losses/classification.py
+++ lio/losses/classification.py
@@ -2,6 +2,7 @@ from typing import Callable, List
 
 import torch
 import torch.nn.functional as F
+from torch.distributions import Categorical
 
 
 def direct_observation_loss(t: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
@@ -52,5 +53,50 @@ def triplet_comparison_loss(activation: Callable = None) -> Callable:
         p_y0 = 1. - p_y1 - p_y2
         p_y = torch.stack((p_y0, p_y1, p_y2), dim=1)
         return F.nll_loss(torch.log(p_y + 1e-32), y)
+
+    return loss
+
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+def soft_bootstrapping_loss(beta: float) -> Callable:
+    # https://arxiv.org/abs/1412.6596
+    # entropy regularization
+
+    def loss(t: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        nll = F.cross_entropy(t, y)
+        reg = -Categorical(probs=F.softmax(t, dim=1)).entropy().mean()
+        return beta * nll + (1. - beta) * reg
+
+    return loss
+
+
+def hard_bootstrapping_loss(beta: float) -> Callable:
+    # https://arxiv.org/abs/1412.6596
+    # log-likelihood regularization
+
+    def loss(t: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        nll = F.cross_entropy(t, y)
+        reg = F.cross_entropy(t, t.argmax(dim=1))
+        return beta * nll + (1. - beta) * reg
+
+    return loss
+
+
+def focal_loss(gamma: float) -> Callable:
+    # https://arxiv.org/abs/1708.02002
+
+    def loss(t: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        return ((1. - F.softmax(t, dim=1)[range(len(y)), y]) ** gamma *
+                F.cross_entropy(t, y, reduction='none')).mean()
+
+    return loss
+
+
+def generalized_cross_entropy_loss(q: float) -> Callable:
+    # https://arxiv.org/abs/1805.07836
+
+    def loss(t: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        return (1. - F.softmax(t, dim=1)[range(len(y)), y] ** q).mean() / q
 
     return loss
